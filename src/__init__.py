69730774c9e4ca187d42b1ed2b3d872ba18886d7
sergiogonzalez-utec <sergio.gonzalez@estudiantes.utec.edu.uy>
2026-04-10 16:50:07 -0300

    Se modifica el archivo __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+from .raw_signal import RawSignal
+from .eeg_signal import EEGSignal
+from .ecg_signal import ECGSignal
+from .emg_signal import EMGSignal
+from .info import Info
+from .eventos import Eventos
+from .anotaciones import Anotaciones
